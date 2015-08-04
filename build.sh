fa0d4a678e7074381b69405a597f6ca6a85517d7
Jason Barnett <jason.w.barnett@gmail.com>
2015-08-03 18:10:52 -0700

    initial commit.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget "https://github.com/pagespeed/ngx_pagespeed/archive/v1.9.32.6-beta.tar.gz"
+wget "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.4.tar.gz"
+wget "https://github.com/openresty/headers-more-nginx-module/archive/v0.26.tar.gz"
