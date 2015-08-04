104461f8763e4ae743050c47845ab51f72d4bf15
Jason Barnett <jason.w.barnett@gmail.com>
2015-08-04 13:11:55 -0700

    updates

--- build.sh
+++ build.sh
@@ -9,10 +9,32 @@ BUILD_OPTIONS=""
 [[ -n ${USER} ]]   || USER="nginx"
 [[ -n ${GROUP} ]]  || GROUP="nginx"
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    BUILD_OPTIONS="${BUILD_OPTIONS} --with-file-aio"
+elif [[ "$OSTYPE" == "darwin" ]]; then
+  echo "hello"
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  echo "hello"
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+  echo "hello"
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+  echo "hello"
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    BUILD_OPTIONS="${BUILD_OPTIONS} --with-file-aio"
+else
+  echo "hello"
+        # Unknown.
+fi
+
 function get_and_extract_artifact () {
   local url=$1
+  local artifact_dir=$2
   local artifact=$(basename ${url})
-  local artifact_dir=$(echo ${artifact} | sed 's|\.tar\.gz||g')
+  [[ -n ${artifact_dir} ]] || artifact_dir=$(echo ${artifact} | sed 's|\.tar\.gz||g')
   if [[ ! -f ${artifact} ]];then
     echo -n "## Fetching ${url}... " 1>&2
     curl -L -s ${url} -O
@@ -30,28 +52,25 @@ function get_and_extract_artifact () {
 function get_pagespeed () {
   local version=1.9.32.6
   local dir=$(get_and_extract_artifact "https://github.com/pagespeed/ngx_pagespeed/archive/v${version}-beta.tar.gz")
-  pushd ngx_pagespeed-${version}-beta &> /dev/null
-  get_and_extract_artifact https://dl.google.com/dl/page-speed/psol/${version}.tar.gz > /dev/null
-  # => ngx_pagespeed-${version}-beta
+  pushd "${dir}" &> /dev/null
+  get_and_extract_artifact "https://dl.google.com/dl/page-speed/psol/${version}.tar.gz" "psol" > /dev/null
   popd &> /dev/null
 
-  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module='${dir}'"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module=${dir}"
 }
 
 function get_ngx_http_auth_pam_module () {
   local version=1.4
   local dir=$(get_and_extract_artifact "https://github.com/stogh/ngx_http_auth_pam_module/archive/v${version}.tar.gz")
-  # => ngx_http_auth_pam_module-${version}
 
-  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module='${dir}'"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module=${dir}"
 }
 
 function get_headers_more_nginx_module () {
   local version=0.26
   local dir=$(get_and_extract_artifact "https://github.com/openresty/headers-more-nginx-module/archive/v${version}.tar.gz")
-  # => headers-more-nginx-module-${version}
 
-  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module='${dir}'"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module=${dir}"
 }
 
 function get_nginx () {
@@ -65,21 +84,21 @@ function get_openssl () {
   local version=1.0.2d
   local dir=$(get_and_extract_artifact "https://www.openssl.org/source/openssl-${version}.tar.gz")
 
-  BUILD_OPTIONS="${BUILD_OPTIONS} --with-openssl='${dir}'"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --with-openssl=${dir}"
 }
 
 function get_zlib () {
   local version=1.2.8
   local dir=$(get_and_extract_artifact "http://zlib.net/zlib-${version}.tar.gz")
 
-  BUILD_OPTIONS="${BUILD_OPTIONS} --with-zlib='${dir}'"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --with-zlib=${dir}"
 }
 
 function get_pcre () {
   local version=8.34
   local dir=$(get_and_extract_artifact "http://downloads.sourceforge.net/project/pcre/pcre/${version}/pcre-${version}.tar.gz")
 
-  BUILD_OPTIONS="${BUILD_OPTIONS} --with-pcre='${dir}'"
+  BUILD_OPTIONS="${BUILD_OPTIONS} --with-pcre=${dir}"
 }
 
 [[ ! -d ${BUILD_DIR} ]] && mkdir ${BUILD_DIR}
@@ -93,12 +112,9 @@ get_ngx_http_auth_pam_module
 get_headers_more_nginx_module
 get_nginx ## This must be last as we depend on this function to place us in the correct directory for ./configure phase
 
-./configure --prefix='${PREFIX}' \
-  --with-cc-opt="-static -static-libgcc" \
-  --with-ld-opt="-static" \
-  --with-cpu-opt=generic \
+./configure --prefix="${PREFIX}" \
   --sbin-path=/usr/sbin/nginx \
-  --conf-path='${PREFIX}/nginx.conf' \
+  --conf-path="${PREFIX}/nginx.conf" \
   --error-log-path=/var/log/nginx/error.log \
   --http-log-path=/var/log/nginx/access.log \
   --pid-path=/var/run/nginx.pid \
@@ -125,7 +141,6 @@ get_nginx ## This must be last as we depend on this function to place us in the 
   --with-http_auth_request_module \
   --with-mail \
   --with-mail_ssl_module \
-  --with-file-aio \
   --with-ipv6 \
   ${BUILD_OPTIONS} \
   --with-cc-opt=-Wno-error
