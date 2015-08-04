39a9f292d7a9032965e935d3cb517322f78ca6d0
Jason Barnett <jason.w.barnett@gmail.com>
2015-08-04 11:54:28 -0700

    building out still

--- build.sh
+++ build.sh
@@ -3,81 +3,102 @@
 
 PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BUILD_DIR=${PWD}/build
+BUILD_OPTIONS=""
+
+[[ -n ${PREFIX} ]] || PREFIX="/etc/nginx"
+[[ -n ${USER} ]]   || USER="nginx"
+[[ -n ${GROUP} ]]  || GROUP="nginx"
 
 function get_and_extract_artifact () {
   local url=$1
   local artifact=$(basename ${url})
   local artifact_dir=$(echo ${artifact} | sed 's|\.tar\.gz||g')
   if [[ ! -f ${artifact} ]];then
-    echo -n "## Fetching ${url}... "
+    echo -n "## Fetching ${url}... " 1>&2
     curl -L -s ${url} -O
-    echo "done"
+    echo "done" 1>&2
   fi
 
-  echo -n "## Extracting artifact ${artifact}... "
+  echo -n "## Extracting artifact ${artifact}... " 1>&2
   [[ -d ${artifact_dir} ]] || mkdir ${artifact_dir}
   tar zxif ${artifact} --strip-components=1 -C ${artifact_dir}
-  echo "done"
+  echo "done" 1>&2
+
+  echo "$(pwd)/${artifact_dir}"
 }
 
 function get_pagespeed () {
   local version=1.9.32.6
-  get_and_extract_artifact "https://github.com/pagespeed/ngx_pagespeed/archive/v${version}-beta.tar.gz"
+  local dir=$(get_and_extract_artifact "https://github.com/pagespeed/ngx_pagespeed/archive/v${version}-beta.tar.gz")
   pushd ngx_pagespeed-${version}-beta &> /dev/null
-  get_and_extract_artifact https://dl.google.com/dl/page-speed/psol/${version}.tar.gz
+  get_and_extract_artifact https://dl.google.com/dl/page-speed/psol/${version}.tar.gz > /dev/null
   # => ngx_pagespeed-${version}-beta
   popd &> /dev/null
+
+  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module='${dir}'"
 }
 
 function get_ngx_http_auth_pam_module () {
   local version=1.4
-  get_and_extract_artifact "https://github.com/stogh/ngx_http_auth_pam_module/archive/v${version}.tar.gz"
+  local dir=$(get_and_extract_artifact "https://github.com/stogh/ngx_http_auth_pam_module/archive/v${version}.tar.gz")
   # => ngx_http_auth_pam_module-${version}
+
+  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module='${dir}'"
 }
 
 function get_headers_more_nginx_module () {
   local version=0.26
-  get_and_extract_artifact "https://github.com/openresty/headers-more-nginx-module/archive/v${version}.tar.gz"
+  local dir=$(get_and_extract_artifact "https://github.com/openresty/headers-more-nginx-module/archive/v${version}.tar.gz")
   # => headers-more-nginx-module-${version}
+
+  BUILD_OPTIONS="${BUILD_OPTIONS} --add-module='${dir}'"
 }
 
 function get_nginx () {
   local version=1.8.0
-  get_and_extract_artifact "http://nginx.org/download/nginx-${version}.tar.gz"
+  local dir=$(get_and_extract_artifact "http://nginx.org/download/nginx-${version}.tar.gz")
+
+  pushd "${dir}" &> /dev/null
 }
 
 function get_openssl () {
   local version=1.0.2d
-  get_and_extract_artifact "https://www.openssl.org/source/openssl-${version}.tar.gz"
+  local dir=$(get_and_extract_artifact "https://www.openssl.org/source/openssl-${version}.tar.gz")
+
+  BUILD_OPTIONS="${BUILD_OPTIONS} --with-openssl='${dir}'"
 }
 
 function get_zlib () {
   local version=1.2.8
-  get_and_extract_artifact "http://zlib.net/zlib-${version}.tar.gz"
+  local dir=$(get_and_extract_artifact "http://zlib.net/zlib-${version}.tar.gz")
+
+  BUILD_OPTIONS="${BUILD_OPTIONS} --with-zlib='${dir}'"
 }
 
 function get_pcre () {
   local version=8.34
-  get_and_extract_artifact "http://downloads.sourceforge.net/project/pcre/pcre/${version}/pcre-${version}.tar.gz"
+  local dir=$(get_and_extract_artifact "http://downloads.sourceforge.net/project/pcre/pcre/${version}/pcre-${version}.tar.gz")
+
+  BUILD_OPTIONS="${BUILD_OPTIONS} --with-pcre='${dir}'"
 }
 
 [[ ! -d ${BUILD_DIR} ]] && mkdir ${BUILD_DIR}
 
 cd ${BUILD_DIR}
+get_openssl
+get_zlib
+get_pcre
 get_pagespeed
 get_ngx_http_auth_pam_module
 get_headers_more_nginx_module
-get_zlib
-get_pcre
-get_nginx
-get_openssl
+get_nginx ## This must be last as we depend on this function to place us in the correct directory for ./configure phase
 
-./configure --prefix=/etc/nginx \
+./configure --prefix='${PREFIX}' \
   --with-cc-opt="-static -static-libgcc" \
   --with-ld-opt="-static" \
   --with-cpu-opt=generic \
   --sbin-path=/usr/sbin/nginx \
-  --conf-path=/etc/nginx/nginx.conf \
+  --conf-path='${PREFIX}/nginx.conf' \
   --error-log-path=/var/log/nginx/error.log \
   --http-log-path=/var/log/nginx/access.log \
   --pid-path=/var/run/nginx.pid \
@@ -87,9 +108,8 @@ get_openssl
   --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
   --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
   --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
-  --user=nginx \
-  --group=nginx \
-  --with-openssl='/home/jbarnett/nginx-compile/openssl-1.0.2d' \
+  --user=${USER} \
+  --group=${GROUP} \
   --with-http_ssl_module \
   --with-http_realip_module \
   --with-http_addition_module \
@@ -107,4 +127,6 @@ get_openssl
   --with-mail_ssl_module \
   --with-file-aio \
   --with-ipv6 \
+  ${BUILD_OPTIONS} \
   --with-cc-opt=-Wno-error
+
