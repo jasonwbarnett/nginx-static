a3aff96961e87717b427655040801cb4d5e1270a
Jason Barnett <jason.w.barnett@gmail.com>
2015-08-04 10:01:34 -0700

    adding more

--- build.sh
+++ build.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
+[[ $DEBUG == true ]] && set -x
 
 PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BUILD_DIR=${PWD}/build
 
+function get_artifact () {
+  local url=$1
+  local artifact=$(basename $url)
+  if [[ ! -f ${artifact} ]];then
+    echo -n "## Fetching ${url}... "
+    curl -L -s ${url} -O
+    echo "done"
+  fi
+}
+
 function get_pagespeed () {
   local version=1.9.32.6
-  wget "https://github.com/pagespeed/ngx_pagespeed/archive/v${version}-beta.tar.gz"
+  get_artifact "https://github.com/pagespeed/ngx_pagespeed/archive/v${version}-beta.tar.gz"
   tar zxf v${version}-beta.tar.gz
-  pushd ngx_pagespeed-${version}-beta
-  wget https://dl.google.com/dl/page-speed/psol/${version}.tar.gz
-  tar xzf ${version}.tar.gz  # extracts to psol/
+  pushd ngx_pagespeed-${version}-beta &> /dev/null
+  get_artifact https://dl.google.com/dl/page-speed/psol/${version}.tar.gz
+  tar zxf ${version}.tar.gz  # extracts to psol/
   # => ngx_pagespeed-${version}-beta
-  popd
+  popd &> /dev/null
 }
 
 function get_ngx_http_auth_pam_module () {
   local version=1.4
-  wget "https://github.com/stogh/ngx_http_auth_pam_module/archive/v${version}.tar.gz"
+  get_artifact "https://github.com/stogh/ngx_http_auth_pam_module/archive/v${version}.tar.gz"
   tar zxf v${version}.tar.gz
   # => ngx_http_auth_pam_module-${version}
 }
 
 function get_headers_more_nginx_module () {
   local version=0.26
-  wget "https://github.com/openresty/headers-more-nginx-module/archive/v${version}.tar.gz"
+  get_artifact "https://github.com/openresty/headers-more-nginx-module/archive/v${version}.tar.gz"
   tar zxf v${version}.tar.gz
   # => headers-more-nginx-module-${version}
+}
+
+function get_nginx () {
+  local version=1.8.0
+  get_artifact "http://nginx.org/download/nginx-${version}.tar.gz"
+  tar zxf nginx-${version}.tar.gz
+}
+
+function get_openssl () {
+  local version=1.0.2d
+  get_artifact "https://www.openssl.org/source/openssl-${version}.tar.gz"
+  tar zxif openssl-${version}.tar.gz
 }
 
 [[ ! -d ${BUILD_DIR} ]] && mkdir ${BUILD_DIR}
@@ -34,3 +57,6 @@ cd ${BUILD_DIR}
 get_pagespeed
 get_ngx_http_auth_pam_module
 get_headers_more_nginx_module
+get_nginx
+get_openssl
+
