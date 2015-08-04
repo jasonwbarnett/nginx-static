bad12ef84ab8da1409f6c20188fdaed351cc588e
Jason Barnett <jason.w.barnett@gmail.com>
2015-08-04 11:22:51 -0700

    added configure line

--- build.sh
+++ build.sh
@@ -72,3 +72,39 @@ get_pcre
 get_nginx
 get_openssl
 
+./configure --prefix=/etc/nginx \
+  --with-cc-opt="-static -static-libgcc" \
+  --with-ld-opt="-static" \
+  --with-cpu-opt=generic \
+  --sbin-path=/usr/sbin/nginx \
+  --conf-path=/etc/nginx/nginx.conf \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log \
+  --pid-path=/var/run/nginx.pid \
+  --lock-path=/var/run/nginx.lock \
+  --http-client-body-temp-path=/var/cache/nginx/client_temp \
+  --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+  --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+  --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+  --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+  --user=nginx \
+  --group=nginx \
+  --with-openssl='/home/jbarnett/nginx-compile/openssl-1.0.2d' \
+  --with-http_ssl_module \
+  --with-http_realip_module \
+  --with-http_addition_module \
+  --with-http_sub_module \
+  --with-http_dav_module \
+  --with-http_flv_module \
+  --with-http_mp4_module \
+  --with-http_gunzip_module \
+  --with-http_gzip_static_module \
+  --with-http_random_index_module \
+  --with-http_secure_link_module \
+  --with-http_stub_status_module \
+  --with-http_auth_request_module \
+  --with-mail \
+  --with-mail_ssl_module \
+  --with-file-aio \
+  --with-ipv6 \
+  --with-cc-opt=-Wno-error
