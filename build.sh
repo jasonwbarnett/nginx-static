b06672f1da02471085d8565e929f377c177fb095
Jason Barnett <jason.w.barnett@gmail.com>
2015-08-03 18:27:36 -0700

    updates

--- build.sh
+++ build.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-wget "https://github.com/pagespeed/ngx_pagespeed/archive/v1.9.32.6-beta.tar.gz"
-wget "https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.4.tar.gz"
-wget "https://github.com/openresty/headers-more-nginx-module/archive/v0.26.tar.gz"
+PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+BUILD_DIR=${PWD}/build
+
+function get_pagespeed () {
+  local version=1.9.32.6
+  wget "https://github.com/pagespeed/ngx_pagespeed/archive/v${version}-beta.tar.gz"
+  tar zxvf v${version}-beta.tar.gz
+  pushd ngx_pagespeed-${version}-beta
+  wget https://dl.google.com/dl/page-speed/psol/${version}.tar.gz
+  tar -xzvf ${version}.tar.gz  # extracts to psol/
+  # => ngx_pagespeed-${version}-beta
+  popd
+}
+
+function get_ngx_http_auth_pam_module () {
+  local version=1.4
+  wget "https://github.com/stogh/ngx_http_auth_pam_module/archive/v${version}tar.gz"
+  tar zxvf v${version}.tar.gz
+  # => ngx_http_auth_pam_module-${version}
+}
+
+function get_ngx_http_auth_pam_module () {
+  local version=0.26
+  wget "https://github.com/openresty/headers-more-nginx-module/archive/v${version}.tar.gz"
+  tar zxvf v${version}.tar.gz
+  # => headers-more-nginx-module-${version}
+}
+
+[[ ! -d ${BUILD_DIR} ]] && mkdir ${BUILD_DIR}
+
+cd ${BUILD_DIR}
+get_pagespeed
+get_ngx_http_auth_pam_module
+get_ngx_http_auth_pam_module
